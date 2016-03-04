e79b1480948e7bb79d26e4d77b2ceb2d2cb40e4a
arsenlol <arsen325@gmail.com>
2016-03-04 14:23:41 +0200

    04.03 pre-final commit

--- js/main.js
+++ js/main.js
@@ -28,3 +28,19 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 //--------------------------------
 
+$('.bitrix-slider').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: '<div class="bit-left"></div>',
+  nextArrow: '<div class="bit-right"></div>',
+});
+
+
+$('.latest-work').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: '<div class="bit-left"></div>',
+  nextArrow: '<div class="bit-right"></div>',
+});
